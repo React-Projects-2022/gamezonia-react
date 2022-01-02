d36b5ed982d832df2ab7431ad60ca1627ad68574
Anartz Mugika Ledo <mugan86@gmail.com>
2022-01-02 12:13:43 +0100

    Implement login feature. Pending add styles

--- src/components/LoginData.js
+++ src/components/LoginData.js
@@ -5,22 +5,16 @@ export const LoginData = ({ email, password }) => {
     variables: { email, password, include: false },
   });
 
-  if (error || loading) {
-    return null;
-  }
+  
   console.log(loading, data);
 
   if (data && data.login.status) {
     window.alert("LOGIN OK");
     window.location.replace("/");
   }
-  console.log("Login MALO");
-  console.log(data.login, loading);
-  //
-
   return (
     <>
-      {loading ? <p>Loading...</p> : <p>{JSON.stringify(data.login.status)}</p>}
+      {loading ? <p>Loading...</p> : (error) ? <p>{error}</p>: <p>{data.login.message}</p>}
     </>
   );
 };
--- src/pages/public/forms/Login.js
+++ src/pages/public/forms/Login.js
@@ -1,32 +1,65 @@
 import { useState } from "react";
 import { LoginData } from "./../../../components/LoginData";
+import { useForm } from "react-hook-form";
+import "./forms.css";
 const Login = () => {
+  const defaultValue ="mugan86@gmail.com";
+  const defaultValueTwo = "user@gamezonia.com";
+  console.log(defaultValueTwo);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
   const [login, setLogin] = useState({
     email: "",
     password: "",
   });
 
-  const passwordsList = ["123456", "1234"];
+  const onSubmit = (data) => {
+    setLogin({ email: data.email, password: data.password });
+  };
 
   return (
     <>
       <h1>Login</h1>
-      <p>Login layout</p>
-      <button
-        onClick={() =>
-          setLogin({
-            email: "mugan86@gmail.com",
-            password: passwordsList[Math.floor(Math.random() * 2)],
-          })
-        }
-      >
-        Click me!
-      </button>
       {login.email !== "" ? (
         <LoginData email={login.email} password={login.password} />
       ) : (
         <p>Haz click para iniciar sesión</p>
-      )}
+      )}  
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="email">Email</label>
+        <input
+          defaultValue={defaultValue}
+          {...register("email", {
+            required: "This field is required",
+            pattern: {
+              value:
+                /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: "Invalid email address",
+            },
+          })}
+        />
+        {errors.email && <p className="validation">{errors.email.message}</p>}
+
+        <label htmlFor="password">Email</label>
+        <input type="password" 
+        defaultValue="123456"
+        placeholder="password" {...register("password", { required: true })} />
+        
+        {errors.password && (
+          <p className="validation">This field is required</p>
+        )}
+        {!errors.password || watch("password") !== " " ? (
+          <input type="submit" />
+        ) : (
+          <input disabled type="submit" />
+        )}
+      </form>
+      
     </>
   );
 };
